fff7f5b737bd98af50d5cff8dd9eb464e849405e
Alex Guo <guojiarui@gmail.com>
2022-09-13 18:46:29 -0700

    projects updates on layout and auth

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
@@ -6,7 +5,7 @@ import Footer from '../components/Footer';
 import Header from '../components/Header';
 import InputForm from '../components/CreateForm';
 import ReportTable from '../components/ReportTable';
-import { useState } from 'react';
+import {useState} from 'react';
 
 export default function Home() {
     const [stores, setStores] = useState([]);
@@ -17,14 +16,14 @@ export default function Home() {
 
     return (
         <div>
-            <Header />
+            {/*<Header/>*/}
 
             <main className="flex flex-col items-center bg-green-100">
-                <InputForm inputHandler={cookieStoreInputHandler} />
-                <ReportTable stores={stores} />
+                <InputForm inputHandler={cookieStoreInputHandler}/>
+                <ReportTable stores={stores}/>
             </main>
 
-            <Footer />
+            {/*<Footer/>*/}
         </div>
     )
 }
